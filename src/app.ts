405d2e0a59a2d2f4218391815c0d6171e52b0b23
Ravshan <jaloliddinov008@gmail.com>
2024-09-26 13:41:47 +0500

    guards

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { CarModule } from './modules';
+import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER, APP_GUARD, APP_PIPE } from '@nestjs/core';
+import { CheckRolesGuard } from './guards';
+import databaseConfig from './config/database.config';
 
 @Module({
-  imports: [CarModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
+    CarModule,
+  ],
+  providers: [
+    {
+      useClass: CheckRolesGuard,
+      provide: APP_GUARD
+    }
+  ],
 })
 export class AppModule {}
